cf1694614c9a1933e702e7303a1bd94625820e74
candicehou <candicehou@tencent.com>
2020-11-28 02:32:29 -0600

    restaurant UI  rate form and chiplist

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router'; 
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { RateDialogComponent} from '../rate-dialog/rate-dialog.component';
+
 export interface Tag {
   name: string;
   freq: number;
@@ -17,6 +20,7 @@ export class RestaurantComponent implements OnInit {
   address = "123 St, Champaign, IL";
   rating = 4;
   category = "boba";
+  userID: number;
 
   restaurant_tags: Tag[] = [
     {name: 'Super cozy', freq: 30},
@@ -25,9 +29,18 @@ export class RestaurantComponent implements OnInit {
     {name: 'Music Live!', freq: 3}
   ];
 
+  //ratings
+  flavorRating: number;
+  environmentRating: number;
+  serviceRating: number;
+
   constructor(private route: ActivatedRoute,
-    private router: Router) {
-    this.getRestaurantData();   
+    private router: Router,
+    public dialog: MatDialog) {
+    this.getRestaurantData(); 
+    let user = JSON.parse(localStorage.getItem('currentUser'));
+    this.userID = user.customer_id;  
+
   }
 
   ngOnInit(): void {
@@ -37,5 +50,24 @@ export class RestaurantComponent implements OnInit {
     this.restaurant_id = this.route.snapshot.paramMap.get("restaurant_id");
     this.restaurant_name = this.route.snapshot.paramMap.get("restaurant_name");
     // todo: get restaurant data api
+  }
+
+  openRateDialog(restaurant_id, restaurantName) {
+    const rateDialogRef = this.dialog.open(RateDialogComponent, {
+      data: { restaurant_name: restaurantName,
+        flavorRating: this.flavorRating,
+        environmentRating: this.environmentRating,
+        serviceRating: this.serviceRating,
+        userID: this.userID
+    }});
+
+    rateDialogRef.afterClosed().subscribe(result => {
+      // console.log(`Rate Dialog result: ${result}`);
+      console.log(restaurant_id + ':'+ restaurantName);
+      console.log("service : "+ result.serviceRating);
+      if(result == "true") {
+        //this.rateRestaurant(restaurant_id, flavor, enviorment, service);
+      }
+    });
   }
 }
